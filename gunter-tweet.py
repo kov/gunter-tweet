7a76ab6be78e6a884fbaaded43f48f568bc19f91
Gustavo Noronha Silva <gustavo@noronha.eti.br>
2013-11-03 14:23:00 -0200

    Eh... bunch of stuff

--- gunter-tweet.py
+++ gunter-tweet.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+import httplib
 import os
 import random
+import re
 import tweepy
 
 import config
 
 
+Nothing = None
+SendReply = 1
+Retweet = 2
+
+
 last_seen_path = os.path.join(os.path.dirname(__file__), 'last-seen')
 last_seen_search_path = os.path.join(os.path.dirname(__file__), 'last-seen-search')
+replies_this_round = 0
+
+
+def too_many_replies():
+    return replies_this_round > 2
 
 
 def get_api():
@@ -44,25 +56,103 @@ def generate_wenks():
     return ' '.join(['Wenk.'] * random.randrange(1, 4))
 
 
-def generate_reply(mention):
-    return '@' + mention.user.screen_name + ' ' + generate_wenks()
+def generate_reply(status):
+    reply = '@' + status.user.screen_name + ' ' + generate_wenks()
+    for update in user_timeline:
+        if update.text == reply:
+            reply = ' Wenk.'
+    return reply
 
 
 def should_wenk():
-    # 10% probability
-    return random.randrange(-9, 1) == 0
+    return random.randrange(-18, 1) == 0
+
+
+def has_gunter(status):
+    text = status.text.lower()
+    if not 'gunter' in text:
+        return False
+
+    if not bool(re.search(r'\bgunter\b', text)) and not bool(re.search(r'\b#gunter\b', text)):
+        return False
+
+    words = ['ice king', 'adventure time', 'adventuretime', '#adventuretime',
+             '#adventuretimeweek', 'hora de aventuras', 'horadeaventuras', '#horadeaventuras']
+
+    for word in words:
+        if re.search(r'\b%s\b' % word, text):
+            return True
+
+    return False
+
+
+def resolve_url(url):
+    connection = httplib.HTTPConnection('t.co')
+    connection.request('GET', url)
+    headers = connection.getresponse().getheaders()
+    for t in headers:
+        if t[0] == 'location':
+            return t[1]
+    return url
+
+
+def resolve_urls(text):
+    urls = re.findall('http://t\.co/[0-9A-Za-z]{10}', text)
+    if not urls:
+        return text
+
+    for url in urls:
+        resolved = resolve_url(url)
+        if url != resolved:
+            text = text.replace(url, resolved)
+
+    return text
+
+
+def what_to_do_with(status):
+    if not has_gunter(status):
+        return Nothing
+
+    text = resolve_urls(status.text)
+
+    for picsite in ['instagram.com', 'pic.twitter.com']:
+        if picsite in text:
+            return Retweet
+
+    return SendReply
+
+
+def already_replied_to(status):
+    for update in user_timeline:
+        if update.in_reply_to_status_id == status.id:
+            return True
+    return False
+
+
+def send_reply(status):
+    if already_replied_to(status):
+        return
+
+    global replies_this_round
+    replies_this_round += 1
+
+    reply = generate_reply(status)
+    try:
+        api.update_status(reply, in_reply_to_status_id=status.id)
+        print 'Replied', reply, 'to', status.text, 'by', status.user.screen_name
+    except tweepy.error.TweepError, e:
+        print status.text, '=>', reply, e.message
 
 
 api = get_api()
+user_timeline = api.user_timeline()
 
 mentions = api.mentions_timeline(since_id=get_last_seen())
 
 if mentions:
     for mention in reversed(mentions):
-        try:
-            api.update_status(generate_reply(mention), in_reply_to_status_id=mention.id)
-        except tweepy.error.TweepError, e:
-            print e.message
+        send_reply(mention)
+
     save_last_seen(mentions)
 elif should_wenk():
     api.update_status(generate_wenks())
@@ -70,10 +160,12 @@ elif should_wenk():
 search_results = api.search('gunter', since_id=get_last_seen_search())
 if search_results:
     for result in search_results:
-        if 'gunter' in result.text.lower() and result.user.screen_name != 'GunterWenkWenk':
-            try:
-                api.update_status(generate_reply(result), in_reply_to_status_id=result.id)
-            except tweepy.error.TweepError, e:
-                print e.message
+        what = what_to_do_with(result)
+        if what == SendReply and not too_many_replies():
+            send_reply(result)
+        elif what == Retweet:
+            result.retweet()
+            print 'Retweeted', result.text, 'by', result.user.screen_name
+
     save_last_seen_search(search_results)
 
